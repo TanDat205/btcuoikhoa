b22753b1456d46317a65c17dec168ed2acf60a06
TanDat205 <dat.qt20p0298@gmail.com>
2023-05-03 19:30:37 +0700

    .

--- gallary.js
+++ gallary.js
@@ -7,6 +7,17 @@ const images = [
     { name: "image6", src: "item3.jpg" },
     { name: "image7", src: "item3.jpg" },
     { name: "image8", src: "item3.jpg" },
+    { name: "image9", src: "img1.png" },
+    { name: "image10", src: "img2.png" },
+    { name: "image11", src: "img3.png" },
+    { name: "image12", src: "img4.png" },
+    { name: "image13", src: "img5.png" },
+    { name: "image14", src: "img6.png" },
+    { name: "image15", src: "img7.png" },
+    { name: "image16", src: "img9.png" },
+    { name: "image17", src: "img10.png" },
+    { name: "image18", src: "img11.png" },
+    { name: "image19", src: "img12.png" },
   ];
 
 localStorage.setItem("images", JSON.stringify(images));
